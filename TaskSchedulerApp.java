4fa07cdde6f33ed55fae2f72f7e1b1334d1c91c8
ericadubie <ericadubie14@gmail.com>
2024-03-24 19:30:52 -0400

    update first class

---
+++ TaskSchedulerApp.java
@@ -0,0 +1,6 @@
+public class TaskSchedulerApp {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("This is my task scheduling cli tool!");
+    }
+}
